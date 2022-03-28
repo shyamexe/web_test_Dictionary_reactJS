b48e3e1dc7afa10b3dbb7664b7c3d9b19bfd1360
shyamexe <shyamjith38@hotmail.com>
2022-03-28 19:44:37 +0530

    added

--- src/HomeView/HomeView.js
+++ src/HomeView/HomeView.js
@@ -1,12 +1,11 @@
 import React from 'react'
-// import MobileFrame from '../MobileFrame/MobileFrame'
 import './HomeView.css'
 
 function HomeView() {
   return (
 
     <div className='home'>
-      <iframe className='home1' src="https://shyamexe.github.io/web_test_Dictionary/#/" height={670} width={335}></iframe>
+      <iframe className='home1' src="https://shyamexe.github.io/web_test_Dictionary/#/" height={652} width={370}></iframe>
     </div>
 
   )
